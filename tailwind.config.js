0469150b69e94d45ffe5101674dd8b27ddbdfe75
Jisun696 <istiakkashem35@gmail.com>
2024-02-18 16:02:36 +0600

    Initial project setup done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-700': '#1DD100',
+        'base-black': '#030712',
+      },
+    },
+  },
+  plugins: [],
+};
