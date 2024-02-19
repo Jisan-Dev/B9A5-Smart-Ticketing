dfcd8da9fd6b009adef17940076c4872399248a0
Jisun696 <istiakkashem35@gmail.com>
2024-02-19 22:22:55 +0600

    Coded all the initially necessary functions and features for the project

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let totalPriceCount = 0;
 
 for (let seat of seats) {
   seat.addEventListener('click', (e) => {
-    let seatNumber = e.target.innerText;
+    const seatNumber = e.target.innerText;
+    const seatNumberElem = document.getElementById(seatNumber);
 
     if (seat.classList.contains('active')) {
       seatTakenCount--;
@@ -16,14 +17,23 @@ for (let seat of seats) {
       seat.classList.remove('active');
       document.getElementById('badge-seatCheckedCount').innerText = seatTakenCount;
       document.getElementById('remainingSeats-count').innerText = remainingSeats;
-      document.getElementById(seatNumber).classList.add('hidden');
+      seatNumberElem.parentNode.removeChild(seatNumberElem);
       totalPriceCount -= 550;
       totalPrice.innerText = totalPriceCount;
+
+      if (document.getElementById('badge-seatCheckedCount').innerText === '0') {
+        document.getElementById('modalOpenerBtn').classList.remove('text-white');
+        document.getElementById('modalOpenerBtn').classList.remove('bg-primary-700');
+        document.getElementById('modalOpenerBtn').classList.remove('cursor-pointer');
+        document
+          .getElementById('modalOpenerBtn')
+          .classList.add('text-gray-600', 'cursor-not-allowed', 'bg-gray-300');
+      }
       return;
     }
 
     if (seatTakenCount >= 4) {
-      alert("can't buy more than 5");
+      alert("can't book more than 4");
       return;
     } else {
       seatTakenCount++;
@@ -40,6 +50,11 @@ for (let seat of seats) {
 
       totalPriceCount += 550;
       totalPrice.innerText = totalPriceCount;
+      document.getElementById('grandPrice').innerText = totalPriceCount;
+
+      document
+        .getElementById('modalOpenerBtn')
+        .classList.add('text-white', 'bg-primary-700', 'cursor-pointer');
     }
 
     if (seatTakenCount <= 4) {
@@ -49,3 +64,24 @@ for (let seat of seats) {
     }
   });
 }
+
+const coupon = document.getElementsByClassName('grow')[0];
+const applyBtn = document.getElementById('applyBtn');
+
+applyBtn.addEventListener('click', () => {
+  // write me code to stop event propagation
+  if (coupon.value === 'NEW15') {
+    const percentage = totalPriceCount * 0.15;
+    const afterPercentage = totalPriceCount - percentage;
+    document.getElementById('couponInputField').classList.add('hidden');
+    document.getElementById('grandPrice').innerText = afterPercentage;
+  } else if (coupon.value === 'Couple 20') {
+    const percentage = totalPriceCount * 0.2;
+    const afterPercentage = totalPriceCount - percentage;
+    document.getElementById('couponInputField').classList.add('hidden');
+    document.getElementById('grandPrice').innerText = afterPercentage;
+  } else {
+    alert('invalid code');
+    return;
+  }
+});
