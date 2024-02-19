de0b783855edd655aa08f463d05dc2d85b95880a
Jisun696 <istiakkashem35@gmail.com>
2024-02-19 15:19:56 +0600

    Refactoring and more functionalities added

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ let seatTakenCount = 0;
 
 for (let seat of seats) {
   seat.addEventListener('click', () => {
+    if (seat.classList.contains('active')) {
+      seatTakenCount--;
+      seat.classList.remove('bg-primary-700');
+      seat.classList.remove('text-white');
+      seat.classList.remove('active');
+      return;
+    }
     seatTakenCount++;
+
     if (seatTakenCount <= 4) {
-      seat.classList.toggle('bg-primary-700');
-      seat.classList.toggle('text-white');
+      seat.classList.add('bg-primary-700');
+      seat.classList.add('text-white');
+      seat.classList.add('active');
+      // console.log(seatTakenCount);
     } else {
       alert("can't buy more than 5");
     }
+
+    console.log('a' + seatTakenCount);
   });
 }
